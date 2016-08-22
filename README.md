777be23fbf8b6ced39754d784fe7f9b4501d4ac7
obedm503 <obedm503@gmail.com>
2016-08-21 19:22:51 -0600

    fiex a bunch of dependencies issues

--- README.md
+++ README.md
@@ -6,7 +6,6 @@
 - **authors:** [AUTHORS.md](./AUTHORS.md)
 - **license:** [LICENSE.md](./LICENSE.md)
 
-----
 >The StudentAccess app was created by students in the Coding and App Development class in 2015. 
 It provides students with a multitude of services such as: checking grades, missing assignments, events, personal balance, staff contact information, and much more. 
 Additionally it also allows students customization within the app with different theme preferences, and two different languages. 
@@ -19,9 +18,28 @@ Pull Requests are always welcome.
 
 ----
 ## Documentation Rules
-----
 - Adhere to tags supported by [JSDoc2md](https://github.com/jsdoc2md/jsdoc-to-markdown).
 - Follow guidelines already in use
 - when linking to an outside source use markdown link tags `[]()` instead of JSDoc link tags `{@link }`
 - more rules tbd
 
+----
+## Get Started with development
+>**make sure these steps happen in the specified order**
+- make sure you have installed and set up
+	- git and some terminal emulator (obedm503 uses [git bash](https://git-scm.com/))
+	- nodejs and npm
+	- gulp, bower, and ionic cli's globally: `npm i -g gulp bower ionic`
+- in the terminal clone the git repository and `cd` into it
+	- `$ git clone git://github.com/ncai-developers/studentaccess.git`
+	- `$ cd studentaccess`
+- install all npm devDependencies, this might take a while
+	- `$ npm install`
+- install all bower dependencies
+	- `$ bower install`
+- config ionic services
+	- `$ ionic config build`
+- build vendor and app files with the default gulp task
+	- `$ gulp`
+- finally serve locally to see the app, this is configured to only watch the html files and the `build/` directory
+	- `$ ionic serve`
--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,10 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util'),
-    bower = require('bower'),
     concat = require('gulp-concat'),
-    sass = require('gulp-sass'),
     cleanCss = require('gulp-clean-css'),
     rename = require('gulp-rename'),
-    sh = require('shelljs'),
     uglify = require('gulp-uglify'),
-    fs = require('fs'),
     gulpJsdoc2md = require('gulp-jsdoc-to-markdown'),
     paths = {
-      sass: ['./scss/**/*.scss'],
       appjs: ['./www/js/app.js','./www/js/config.js','./www/components/**/*.js'],
       appcss: ['./www/components/**/*.css'],
       vendorjs: [
@@ -34,8 +28,8 @@ var gulp = require('gulp'),
       ]
     };
 
-gulp.task('default', ['docs','appcss','appjs','vendorjs','vendorcss']);
-gulp.task('buildapp',['docs','appcss', 'appjs']);
+gulp.task('default', ['appcss','appjs','vendorjs','vendorcss']);
+gulp.task('buildapp',['appcss', 'appjs']);
 gulp.task('buildvendor',['vendorcss', 'vendorjs']);
 
 gulp.task('docs', function () {
@@ -71,39 +65,7 @@ gulp.task('vendorcss', function() {
     .pipe(gulp.dest('./www/build/'));
 });
 
-gulp.task('sass', function(done) {
-  gulp.src('./scss/ionic.app.scss')
-    .pipe(sass())
-    .on('error', sass.logError)
-    .pipe(gulp.dest('./www/css/'))
-    .pipe(cleanCss())
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
-    .on('end', done);
-});
-
 gulp.task('watch', function() {
-  //gulp.watch(paths.sass, ['sass']);
   gulp.watch(paths.appjs, ['appjs']);
   gulp.watch(paths.appcss, ['appcss']);
-});
-
-gulp.task('install', ['git-check'], function() {
-  return bower.commands.install()
-    .on('log', function(data) {
-      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
-    });
-});
-
-gulp.task('git-check', function(done) {
-  if (!sh.which('git')) {
-    console.log(
-      '  ' + gutil.colors.red('Git is not installed.'),
-      '\n  Git, the version control system, is required to download Ionic.',
-      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
-      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
-    );
-    process.exit(1);
-  }
-  done();
 });
