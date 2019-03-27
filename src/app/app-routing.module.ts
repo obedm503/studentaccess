157b03a6b826a0a0f38afc80f2021f9ecf7ddaf1
obedm503 <obedm503@gmail.com>
2019-03-27 01:01:20 -0500

    extract routing definitions to its own module as per angular conventions

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,55 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'profile',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    loadChildren: './pages/login/login.module#LoginModule',
+  },
+  {
+    path: 'profile/records',
+    loadChildren: './pages/records/records.module#RecordsModule',
+  },
+  {
+    path: 'profile',
+    loadChildren: './pages/profile/profile.module#ProfileModule',
+  },
+  {
+    path: 'homework',
+    loadChildren: './pages/homework/homework.module#HomeworkModule',
+  },
+  {
+    path: 'grades/:class',
+    loadChildren:
+      './pages/grades-detail/grades-detail.module#GradesDetailModule',
+  },
+  {
+    path: 'grades',
+    loadChildren: './pages/grades/grades.module#GradesModule',
+  },
+  {
+    path: 'events',
+    loadChildren: './pages/events/events.module#EventsModule',
+  },
+  {
+    path: 'cafeteria',
+    loadChildren: './pages/cafeteria/cafeteria.module#CafeteriaModule',
+  },
+  {
+    path: 'staff',
+    loadChildren: './pages/staff/staff.module#StaffModule',
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+  ],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
