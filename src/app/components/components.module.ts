953056ba291e0528ab7b65744053b16dad115a4a
obedm503 <obedm503@gmail.com>
2019-03-27 10:42:45 -0500

    components use IonicModule

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 import { GradeBadge } from './grade-badge/grade-badge';
 import { Ribbon } from './ribbon/ribbon';
 
 @NgModule({
   declarations: [Ribbon, GradeBadge],
   exports: [Ribbon, GradeBadge],
+  imports: [IonicModule],
 })
 export class ComponentsModule {}
